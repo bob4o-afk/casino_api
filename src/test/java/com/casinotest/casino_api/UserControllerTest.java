3a37708c65db242c5f379f2c8239cf167c37faf1
bob4o-afk <bobkatasad@abv.bg>
2023-12-19 11:38:12 +0200

    Updating Tests

--- src/test/java/com/casinotest/casino_api/UserControllerTest.java
+++ src/test/java/com/casinotest/casino_api/UserControllerTest.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Collections;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
